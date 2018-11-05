a6490ee58e8cc98824bd55ce8e6ee4713db12de8
LuisFelipe <bpl_@outlook.com>
2018-11-05 09:20:30 -0300

    Model problem solved!

--- website/consulta/models.py
+++ website/consulta/models.py
@@ -7,6 +7,9 @@ class aluno(models.Model):
     sexo = models.CharField(max_length=10, blank=True, null=True) #blank=True, null=True campo pode ser vazio
     email = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nome
+
 class tipo_ingresso(models.Model):
     id = models.IntegerField(default=11, primary_key=True)
     tipoIngresso = models.CharField(max_length=50)
@@ -15,6 +18,9 @@ class departamento(models.Model):
     id = models.IntegerField(default=11, primary_key=True)
     nome = models.CharField(max_length=20)
     bloco = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.nome
 
 class situacao_matricula(models.Model):
     id = models.IntegerField(default=11, primary_key=True)
@@ -25,3 +31,6 @@ class curso(models.Model):
     id_dpto = models.ForeignKey(departamento, on_delete=models.CASCADE)
     nome = models.CharField(max_length=20)
     descricao = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nome
